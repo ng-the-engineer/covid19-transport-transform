dfbfb9927dc4a6499e9190db0ba81d10d2c9ca00
Anthony Ng <engineer.anthony.ng@gmail.com>
2020-05-17 21:59:31 +0800

    Add format validation

---
+++ validateFormat.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+)
+
+// -----------------------------------------------------------
+// Assumption
+// 1. No header column
+// 2. Four columns; Username; Identifier;First name;Last name
+// -----------------------------------------------------------
+
+func validateFormat(rd *csv.Reader) (bool, string) {
+	rowCnt := 0
+
+	columnCount := 4
+
+	problemRows := []string{}
+
+	for {
+		row, err := rd.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rowArr := strings.Split(row[0], ";")
+
+		if len(rowArr) != columnCount {
+			problemRows = append(problemRows, strconv.Itoa(rowCnt))
+		}
+
+		rowCnt++
+	}
+
+	isValid := (len(problemRows) == 0)
+	output := strings.Join(problemRows, ",")
+
+	return isValid, output
+}
